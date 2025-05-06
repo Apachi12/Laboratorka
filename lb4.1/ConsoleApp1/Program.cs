9c334b477c22ca475407c12fa1a9923e4160f812
Apachi12 <daniildzuba31@gmail.com>
2025-05-06 23:42:20 +0300

    Обновление проекта

---
+++ lb4.1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string[] testEmails = {
+            "user12@example.com",  // допустимый
+            "user@gmail.com",      // допустимый
+            "bug@@@com.ru",        // недопустимый
+            "@gmail.com",          // недопустимый
+            "Just Text2"           // недопустимый
+        };
+
+        string pattern = @"^[a-zA-Z][a-zA-Z0-9._%+-]*@[a-zA-Z0-9.-]+\.(com|localhost)$";
+
+        foreach (string email in testEmails)
+        {
+            bool isMatch = Regex.IsMatch(email, pattern);
+            Console.WriteLine($"{email} → {(isMatch ? "допустимий" : "недопустимий")}");
+        }
+    }
+}
